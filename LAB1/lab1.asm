6b9f34c4cf9e24f1fcb64dbfcd3a0d902adbc616
samuelc3 <samuelc3@39c386cc-3532-487e-8021-ffb347eaac5c>
2017-01-22 04:41:32 +0000

    test lab1

--- LAB1/lab1.asm
+++ LAB1/lab1.asm
@@ -1,39 +1,33 @@
- Lab 1 
+; Lab 1 
  ; assignment: develop a code to print a value stored in a register 
  ;             as a hexadecimal number to the monitor
  ; algorithm: turnin each group of four bits into a digit
  ;            calculate the corresponding ASCII character;
  ;            print the character to the monitor
 
- .ORIG x3000
-		
-		AND R3, #0
-		ADD R3, xBRUH
-		STI R3, VALUE
-		ADD R0, #4
-MEME	ADD R1, #4
-		AND R4, #0
-
-		
-		ADD R5, R5, R3
-		AND R5 X8000
-		BRn ECE
-		
-SHIFT						; take digits from left side (R3) and load from right side into R4
-		
-		ADD R3, R3, R3
-		ADD R1, #-1
-		BRp SHIFT
-ECE								; if MSB is 0
-
-		ADD R5, R5, R4
-PRINT	ADD R5, #-9
-		BRzp POOP
-		ADD R4, #65
+.ORIG x3000
+AND	R5,R5,#0
+ADD R6,R6,#4
+AND R1,R1,#0
+MEME	ADD R1,R1,#4
+AND R4,R4,#0
+ADD R5,R5,R3
+AND R5,R5,#-1
+SHIFT	BRzp ECE	;take digits from left side (R3) and load from right side into R4
+ADD R4,R4,#1
+ADD R4,R4,R4
+ADD R1,R1,#-1
+BRp SHIFT
+ECE		ADD R5,R5,R5						; if MSB is 0
+ADD R5,R5,R4
+PRINT	ADD R5,R5,#-9
+BRzp POOP
+ADD R4,R4,xA
+ADD R4,R4,#-10
 POOP	OUT
-		AND R4, #0
-		ADD R0, #-1
-		BRp MEME
+AND R4,R4,#0
+ADD R6,R6,#-1
+BRp MEME
 
 
 
@@ -42,7 +36,6 @@ POOP	OUT
 
  ; program data section starts here
 
-VALUE	.FILL	X4000
 
  .END
 
