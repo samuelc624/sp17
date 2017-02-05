7edb666b7208d041e73d0e577b2677c356481651
samuelc3 <samuelc3@39c386cc-3532-487e-8021-ffb347eaac5c>
2017-02-05 22:35:06 +0000

    worked on mp2

--- MP2/prog2.asm
+++ MP2/prog2.asm
@@ -1,4 +1,5 @@
-;
+;ADD R5, R5, #0						USE THIS AFTER EVERY SUBROUTINE
+;BRp	FAIL
 ;
 ;
 .ORIG x3000
@@ -16,9 +17,79 @@
 
 
 
+
+
+
+
+
+
+
+
+BRnzp	DONE
+
+FAIL	LEA R0, STRING
+	PUTS
+DONE	HALT
+
+
+
+
+STRING	.STRINGZ "Invalid Expression."
+
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;R3- value to print in hexadecimal
 PRINT_HEX
+
+	
+	LDR R3,R6,#0	;let value be stored in R4
+	ADD R1,R1,#4	;digit counter
+
+DIGIT	BRz NEXT	;checks if digit counter is 0
+	AND R0,R0,#0	;resets used registers for next digit
+	AND R4,R4,#0
+
+	AND R2,R2,#0
+	ADD R2,R2,#4	;bit counter
+
+BIT	BRz PRINT	
+	ADD R4,R4,R4	;shift digit left
+	ADD R3,R3,#0	;set nzp
+	BRn ONEBIT	;check most significant bit
+	BRzp ZEROBIT
+
+ONEBIT	ADD R4,R4,#1	;if negative, add 1
+	BRnzp NXTDGT
+
+ZEROBIT	ADD R4,R4,#0	;if positive, add 0
+	BRnzp NXTDGT
+
+NXTDGT	ADD R3,R3,R3	;shift R4 left
+	ADD R2,R2,#-1	;decrement bit counter
+	BRp BIT	;less than 4 digits from R4
+	BRnz PRINT	;if 4 bits taken, go to output
+	
+PRINT	ADD R4,R4,#-10	;calculate offset by subtracting -10 or xA
+	BRn NMBR	;if negative, its a number
+	BRzp ALPH	;if zero or positive, then letter
+	
+NMBR	ADD R4,R4,#10	;get number offset
+	LD R0,NUM	;load ASCII 0
+	ADD R0,R0,R4	;add offset
+	OUT		;display
+	ADD R1,R1,#-1	;decrement digit counter
+	BRnzp DIGIT	;loop
+	
+ALPH 	LD R0,LETTER	;load ASCII A 
+	ADD R0,R0,R4	;add offset
+	OUT		;display
+	ADD R1,R1,#-1	;decrement digit counter
+	BRnzp DIGIT	;loop
+
+
+
+
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;R0 - character input from keyboard
@@ -30,36 +101,164 @@ EVALUATE
 ;your code goes here
 
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+
+
+
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;input R3, R4
 ;out R0
 PLUS	
 ;your code goes here
+
+
+
+JSR POP
+ADD R5, R5, #0
+BRp	RETURN
+ADD R4, R0, #0
+JSR POP
+ADD R5, R5, #0
+BRp	RETURN
+ADD R3, R0, #0
+AND R0, R0, #0
+ADD R0, R3, R4
+RETURN	RET
 	
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;input R3, R4
 ;out R0
 MIN	
 ;your code goes here
+
+JSR POP
+ADD R5, R5, #0
+BRp	RETURN	
+ADD R4, R0, #0
+JSR POP
+ADD R5, R5, #0
+BRp	RETURN
+ADD R3, R0, #0
+NOT R4, R4
+ADD R4, R4, #1
+ADD R0, R3, R4
+JSR PUSH
+RETURN	RET
+
+
+
+
 	
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;input R3, R4
 ;out R0
 MUL	
 ;your code goes here
+
+	JSR POP
+	ADD R5, R5, #0
+	BRp	RETURN
+	ADD R4, R0, #0
+	JSR POP
+	ADD	R5, R5, #0
+	BRp	RETURN
+	AND R3, R4, #O
+LOOP_M	ADD R4, R4, R0
+	ADD R3, R3, #-1
+	BRp LOOP_M
+	ADD R0, R4, #0
+	JSR PUSH
+RETURN	RET
+
+
 	
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;input R3, R4
 ;out R0
-;DIV	
+DIV	
 ;your code goes here
-	
-	
+
+
+
+JSR POP
+ADD R5, R5, #0
+BRp	RET
+ADD R4, R0, #0
+JSR POP
+ADD R5, R5, #0
+BRp	RET
+ADD R3, R0, #0
+AND R0, R0, #0
+AND R1, R1, #0
+ADD R1, R3, R1
+ZERO	ADD R0, R0, #1
+NOT R4, R4
+ADD R4, R4, #1
+ADD R1, R1, R4
+BRzp	ZERO
+ADD R0, R0, #-1
+NOT R4, R4
+ADD R4, R4, #1
+ADD R1, R1, R4
+RETURN	RET
+
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;input R3, R4
 ;out R0
 ;EXP
 ;your code goes here
+	
+	
+	
+ST R2, SAVE_R2
+	JSR POP
+	ADD R5, R5, #0
+	BRp	RETURN
+	ADD R4, R0, #0
+	JSR POP
+	ADD R5, R5, #0
+	BRp	RETURN
+	ADD R3, R0, #0
+	AND R3, R3, #0
+LOOP_OU	ADD R2, R0, #0
+LOOP_IN	ADD R3, R3, R0
+	ADD R2, R2, #-1
+	BRp LOOP_IN
+	ADD R4, R4, #-1
+	BRp LOOP_OU
+	ADD R0, R3, #0
+	JSR PUSH
+	LD R2, SAVE_R2
+RETURN	RET
+SAVE_R2	.BLKW #1
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
+
+
+	
+	
+	
+	
+	
 	
 ;IN:R0, OUT:R5 (0-success, 1-fail/overflow)
 ;R3: STACK_END R4: STACK_TOP
