832308dbb9b0486644beace499d24f7b0df5d3f8
clin99 <clin99@39c386cc-3532-487e-8021-ffb347eaac5c>
2017-02-10 01:55:34 +0000

    Upload MP3

---
+++ MP3/mp3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  int row_index;
+
+  printf("Enter the row index : ");
+  scanf("%d",&row_index);
+
+  // Your code starts from here
+
+  return 0;
+}
