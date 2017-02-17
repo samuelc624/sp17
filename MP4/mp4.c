5bbb7d78a33e7e38e6e7165b0aaba35c61426795
clin99 <clin99@39c386cc-3532-487e-8021-ffb347eaac5c>
2017-02-17 01:43:09 +0000

    Upload MP4

---
+++ MP4/mp4.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+double abs_double(double y)
+{
+    //Change this to return the absolute value of y i.e |y|
+    return 0;
+}
+
+double fx_val(double a, double b, double c, double d, double e, double x)
+{
+    //Change this to return the value of the polynomial at the value x
+    return 0;
+}
+
+double fx_dval(double a, double b, double c, double d, double e, double x)
+{
+    //Change this to return the value of the derivative of the polynomial at the value x
+    return 0;
+}
+
+double fx_ddval(double a, double b, double c, double d, double e, double x)
+{
+    //Change this to return the value of the double derivative of the polynomial at the value x
+    return 0;
+}
+
+
+double newrfind_halley(double a, double b, double c, double d, double e, double x)
+{
+    //Change this to return the root found starting from the initial point x using Halley's method
+    return 0;
+}
+
+int rootbound(double a, double b, double c, double d, double e, double r, double l)
+{
+    //Change this to return the upper bound on the number of roots of the polynomial in the interval (l, r)
+    return 0;
+}
+
+int main(int argc, char **argv)
+{
+	double a, b, c, d, e, l, r;
+	FILE *in;
+
+	if (argv[1] == NULL) {
+		printf("You need an input file.\n");
+		return -1;
+	}
+	in = fopen(argv[1], "r");
+	if (in == NULL)
+		return -1;
+	fscanf(in, "%lf", &a);
+	fscanf(in, "%lf", &b);
+	fscanf(in, "%lf", &c);
+	fscanf(in, "%lf", &d);
+	fscanf(in, "%lf", &e);
+	fscanf(in, "%lf", &l);
+	fscanf(in, "%lf", &r);
+    
+    //The values are read into the variable a, b, c, d, e, l and r.
+    //You have to find the bound on the number of roots using rootbound function.
+    //If it is > 0 try to find the roots using newrfind function.
+    //You may use the fval, fdval and fddval funtions accordingly in implementing the halleys's method.
+    
+    
+    fclose(in);
+    
+    return 0;
+}
