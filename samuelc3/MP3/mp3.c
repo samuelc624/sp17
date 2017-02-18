56ca63d02c5fb256aa9ea818733a5ca1717c1b39
samuelc3 <samuelc3@39c386cc-3532-487e-8021-ffb347eaac5c>
2017-02-18 19:15:35 +0000

    lab3

--- samuelc3/MP3/mp3.c
+++ samuelc3/MP3/mp3.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+double factorial(int x)
+{	int i = 1;
+	double fact = 1;
+	if (x==0){
+		return 1;
+	}
+	else {
+		for (i=1; i<=x; i++){
+			fact *= i;
+			}
+		return fact;
+	}
+	
+}
+
 int main()
 {
   int row_index;
@@ -9,6 +24,19 @@ int main()
   scanf("%d",&row_index);
 
   // Your code starts from here
-
+	int n = row_index;
+	double  k, denom, kfact, denomfact, nfact, value;
+	nfact = factorial(n);
+	for (k=0; k<=n; k++){
+		denom = n-k;
+		denomfact = factorial(denom);
+		kfact = factorial(k);
+		value = (nfact)/(kfact*denomfact);
+		printf("%.0f", value);
+		if (k<n){
+		printf(" ");
+		}
+	}
+  printf("\n");
   return 0;
 }
