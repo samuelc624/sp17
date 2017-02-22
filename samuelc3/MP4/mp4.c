51011d7f8f33aace52b9674450926c0b2f432656
samuelc3 <samuelc3@39c386cc-3532-487e-8021-ffb347eaac5c>
2017-02-22 22:24:28 +0000

    mp4_2

--- samuelc3/MP4/mp4.c
+++ samuelc3/MP4/mp4.c
@@ -39,7 +39,7 @@ double newrfind_halley(double a, double b, double c, double d, double e, double 
     return 0;
 }
 
-int rootbound(double a, double b, double c, double d, double e, double r, double l, double p_l, double first_l, double second_l, double third_l, double fourth_l, double fifth_l, double p_r double first_r, double second_r, double third_r, double fourth_r, double fifth_r, double v_l, double v_r)
+int rootbound(double a, double b, double c, double d, double e, double r, double l, double p_l, double first_l, double second_l, double third_l, double fourth_l, double fifth_l, double p_r double first_r, double second_r, double third_r, double fourth_r, double fifth_r, double v_l, double v_r, double upper)
 {
     //Change this to return the upper bound on the number of roots of the polynomial in the interval (l, r)
     first_l=a;
@@ -48,10 +48,50 @@ int rootbound(double a, double b, double c, double d, double e, double r, double
     fourth_l=4*a*l*l*l+3*b*l*l+2*c*l+d;
     fifth_l=a*l*l*l*l+b*l*l*l+c*l*l+d*l+e;
       p_l = (first_l)x*x*x*x+(second_l)*x*x*x+(third_l)*x*x+(fourth_l)*x+(fifth_l);
+  if ((first_l ^ second_l || first_l ^ third_l || first_l ^ fourth_l || first_l ^ fifth_l) < 0)
+      {
+        v_l=v_l++;
+      }
+  if ((second_l ^ third_l || second_l ^ fourth_l || second_l ^ fifth_l) < 0) {
+        v_l=v_l++;
+      }
+  if ((third_l ^ fourth_l || third_l ^ fifth_l) < 0) {
+        v_l=v_l++;
+      }
+  if ((fourth_l ^ fifth_l) < 0) {
+        v_l=v_l++;
+      }
 
 
+    first_r=a;
+    second_r=4*a*r+b;
+    third_r=6*a*r*r+3*b*r+c;
+    fourth_r=4*a*r*r*r+3*b*r*r+2*c*r+d;
+    fifth_r=a*r*r*r*r+b*r*r*r+c*r*r+d*r+e;
+      p_r = (first_r)x*x*x*x+(second_r)*x*x*x+(third_r)*x*x+(fourth_r)*x+(fifth_r);
+  if ((first_r ^ second_r || first_r ^ third_r || first_r ^ fourth_r || first_r ^ fifth_r) < 0)
+      {
+        v_r=v_r++;
+      }
+  if ((second_r ^ third_r || second_r ^ fourth_r || second_r ^ fifth_r) < 0) {
+        v_r=v_r++;
+      }
+  if ((third_r ^ fourth_r || third_r ^ fifth_r) < 0) {
+        v_r=v_r++;
+      }
+  if ((fourth_r ^ fifth_r) < 0) {
+        v_r=v_r++;
+      }
 
-    return 0;
+    v_l-v_r=upper;
+
+  if (upper < 0)
+          {
+            upper = -upper;
+          }
+
+
+    return upper;
 }
 
 int main(int argc, char **argv)
