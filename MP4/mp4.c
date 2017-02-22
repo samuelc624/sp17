39a71dfc3d9c3d3eab4c56b2eae5f777b36d6ad5
samuelc3 <samuelc3@39c386cc-3532-487e-8021-ffb347eaac5c>
2017-02-22 21:27:46 +0000

    mp4_1

--- MP4/mp4.c
+++ MP4/mp4.c
@@ -5,37 +5,52 @@
 double abs_double(double y)
 {
     //Change this to return the absolute value of y i.e |y|
-    return 0;
+    if (y < 0)
+      {
+        y=-y;
+      }
+    return y;
 }
 
 double fx_val(double a, double b, double c, double d, double e, double x)
 {
     //Change this to return the value of the polynomial at the value x
-    return 0;
+
+    return a*x*x*x*x+b*x*x*x+c*x*x+d*x*e;
 }
 
 double fx_dval(double a, double b, double c, double d, double e, double x)
 {
     //Change this to return the value of the derivative of the polynomial at the value x
-    return 0;
+    return 4*a*x*x*x+3*b*x*x+2*x*c+d;
 }
 
 double fx_ddval(double a, double b, double c, double d, double e, double x)
 {
     //Change this to return the value of the double derivative of the polynomial at the value x
-    return 0;
+    return 12*a*x*x+6*x*b+2*c;
 }
 
 
 double newrfind_halley(double a, double b, double c, double d, double e, double x)
 {
     //Change this to return the root found starting from the initial point x using Halley's method
+
     return 0;
 }
 
-int rootbound(double a, double b, double c, double d, double e, double r, double l)
+int rootbound(double a, double b, double c, double d, double e, double r, double l, double p_l, double first_l, double second_l, double third_l, double fourth_l, double fifth_l, double p_r double first_r, double second_r, double third_r, double fourth_r, double fifth_r, double v_l, double v_r)
 {
     //Change this to return the upper bound on the number of roots of the polynomial in the interval (l, r)
+    first_l=a;
+    second_l=4*a*l+b;
+    third_l=6*a*l*l+3*b*l+c;
+    fourth_l=4*a*l*l*l+3*b*l*l+2*c*l+d;
+    fifth_l=a*l*l*l*l+b*l*l*l+c*l*l+d*l+e;
+      p_l = (first_l)x*x*x*x+(second_l)*x*x*x+(third_l)*x*x+(fourth_l)*x+(fifth_l);
+
+
+
     return 0;
 }
 
@@ -58,14 +73,14 @@ int main(int argc, char **argv)
 	fscanf(in, "%lf", &e);
 	fscanf(in, "%lf", &l);
 	fscanf(in, "%lf", &r);
-    
+
     //The values are read into the variable a, b, c, d, e, l and r.
     //You have to find the bound on the number of roots using rootbound function.
     //If it is > 0 try to find the roots using newrfind function.
     //You may use the fval, fdval and fddval funtions accordingly in implementing the halleys's method.
-    
-    
+
+
     fclose(in);
-    
+
     return 0;
 }
