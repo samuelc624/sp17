9df67f956b5204ea9c62f80ff6128bb870bfd523
samuelc3 <samuelc3@39c386cc-3532-487e-8021-ffb347eaac5c>
2017-02-23 03:05:25 +0000

    finished mp4

--- samuelc3/MP4/mp4.c
+++ samuelc3/MP4/mp4.c
@@ -4,62 +4,74 @@
 
 double abs_double(double y)
 {
+	if (y<0){
+		y=-y;
+		}
     //Change this to return the absolute value of y i.e |y|
-    if (y < 0)
-      {
-        y=-y;
-      }
     return y;
 }
 
 double fx_val(double a, double b, double c, double d, double e, double x)
 {
     //Change this to return the value of the polynomial at the value x
-
-    return a*x*x*x*x+b*x*x*x+c*x*x+d*x*e;
+    return a*x*x*x*x+b*x*x*x+c*x*x+d*x+e;
 }
 
 double fx_dval(double a, double b, double c, double d, double e, double x)
 {
-    //Change this to return the value of the derivative of the polynomial at the value x
-    return 4*a*x*x*x+3*b*x*x+2*x*c+d;
+	//Change this to return the value of the derivative of the polynomial at the value x
+    return 4*a*x*x*x+3*b*x*x+2*c*x+d;
 }
-
 double fx_ddval(double a, double b, double c, double d, double e, double x)
 {
     //Change this to return the value of the double derivative of the polynomial at the value x
-    return 12*a*x*x+6*x*b+2*c;
+    return 12*a*x*x+6*b*x+2*c;
 }
-
 
 double newrfind_halley(double a, double b, double c, double d, double e, double x)
 {
-    //Change this to return the root found starting from the initial point x using Halley's method
+	int i = 0;
+	int flag = 0;
+double prev_x = x;
+double new_x;
+    for (i=0; i<10000; i++){
+		            new_x = prev_x - (2*fx_val(a,b,c,d,e,prev_x)*fx_dval(a, b,c,d,e,prev_x))/((2*(abs_double(fx_dval(a,b,c,d,e,prev_x)))*(abs_double(fx_dval(a,b,c,d,e,prev_x))))-fx_val(a,b,c,d,e,prev_x)*fx_ddval(a,b,c,d,e,prev_x));
+	   if (abs_double((new_x-prev_x))<0.000001){
+			    return new_x;
+      			    flag = 1;
+			             break;
+                            }
+		prev_x = new_x;
+		}
 
-    return 0;
+    //Change this to return the root found starting from the initial point x using Halley's method
+	if (flag == 0){
+	return 8013.60561;}
 }
 
-int rootbound(double a, double b, double c, double d, double e, double r, double l, double p_l, double first_l, double second_l, double third_l, double fourth_l, double fifth_l, double p_r double first_r, double second_r, double third_r, double fourth_r, double fifth_r, double v_l, double v_r, double upper)
+int rootbound(double a, double b, double c, double d, double e, double r, double l)
 {
+    double first_l, second_l, third_l, fourth_l, fifth_l, v_l, first_r, second_r, third_r, fourth_r, fifth_r, v_r, upper;
+
     //Change this to return the upper bound on the number of roots of the polynomial in the interval (l, r)
     first_l=a;
     second_l=4*a*l+b;
     third_l=6*a*l*l+3*b*l+c;
     fourth_l=4*a*l*l*l+3*b*l*l+2*c*l+d;
     fifth_l=a*l*l*l*l+b*l*l*l+c*l*l+d*l+e;
-      p_l = (first_l)x*x*x*x+(second_l)*x*x*x+(third_l)*x*x+(fourth_l)*x+(fifth_l);
-  if ((first_l ^ second_l || first_l ^ third_l || first_l ^ fourth_l || first_l ^ fifth_l) < 0)
+    //p_l = (first_l)x*x*x*x+(second_l)*x*x*x+(third_l)*x*x+(fourth_l)*x+(fifth_l);
+  if ((first_l * second_l) < 0)
       {
-        v_l=v_l++;
+        v_l++;
       }
-  if ((second_l ^ third_l || second_l ^ fourth_l || second_l ^ fifth_l) < 0) {
-        v_l=v_l++;
+  if ((second_l * third_l) < 0) {
+        v_l++;
       }
-  if ((third_l ^ fourth_l || third_l ^ fifth_l) < 0) {
-        v_l=v_l++;
+  if ((third_l * fourth_l) < 0) {
+        v_l++;
       }
-  if ((fourth_l ^ fifth_l) < 0) {
-        v_l=v_l++;
+  if ((fourth_l * fifth_l) < 0) {
+        v_l++;
       }
 
 
@@ -68,35 +80,32 @@ int rootbound(double a, double b, double c, double d, double e, double r, double
     third_r=6*a*r*r+3*b*r+c;
     fourth_r=4*a*r*r*r+3*b*r*r+2*c*r+d;
     fifth_r=a*r*r*r*r+b*r*r*r+c*r*r+d*r+e;
-      p_r = (first_r)x*x*x*x+(second_r)*x*x*x+(third_r)*x*x+(fourth_r)*x+(fifth_r);
-  if ((first_r ^ second_r || first_r ^ third_r || first_r ^ fourth_r || first_r ^ fifth_r) < 0)
+    //p_r = (first_r)x*x*x*x+(second_r)*x*x*x+(third_r)*x*x+(fourth_r)*x+(fifth_r);
+  if ((first_r * second_r) < 0)
       {
-        v_r=v_r++;
+        v_r++;
       }
-  if ((second_r ^ third_r || second_r ^ fourth_r || second_r ^ fifth_r) < 0) {
-        v_r=v_r++;
+  if ((second_r * third_r) < 0) {
+        v_r++;
       }
-  if ((third_r ^ fourth_r || third_r ^ fifth_r) < 0) {
-        v_r=v_r++;
+  if ((third_r * fourth_r) < 0) {
+        v_r++;
       }
-  if ((fourth_r ^ fifth_r) < 0) {
-        v_r=v_r++;
+  if ((fourth_r * fifth_r) < 0) {
+        v_r++;
       }
 
-    v_l-v_r=upper;
-
-  if (upper < 0)
-          {
-            upper = -upper;
-          }
-
+    upper = v_l-v_r;
+    upper = abs_double(upper);
 
     return upper;
+
 }
 
 int main(int argc, char **argv)
 {
 	double a, b, c, d, e, l, r;
+	double x, rootz, sam;
 	FILE *in;
 
 	if (argv[1] == NULL) {
@@ -118,9 +127,24 @@ int main(int argc, char **argv)
     //You have to find the bound on the number of roots using rootbound function.
     //If it is > 0 try to find the roots using newrfind function.
     //You may use the fval, fdval and fddval funtions accordingly in implementing the halleys's method.
-
-
+    rootz = rootbound(a,b,c,d,e,r,l);
+    	if (rootz==0){
+    		printf("The polynomial has no roots in the given interval.\n");
+    		fclose(in);
+    		return 0;
+    		}
+    else{
+    for (l; l<=r; l+=0.5){
+    	x=l;
+ 	sam = newrfind_halley(a,b,c,d,e,x);
+ 	if (sam == 8013.60561){
+	printf("No roots found.\n");
+ 	}
+ 	else{
+ 	printf("Root found: %lf \n", sam);
+ 	}
+    }
     fclose(in);
-
     return 0;
+    }
 }
