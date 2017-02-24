1fe7728563a433447766202305dcc609de75a037
samuelc3 <samuelc3@39c386cc-3532-487e-8021-ffb347eaac5c>
2017-02-24 19:36:42 +0000

    lab5

--- samuelc3/LAB5/dice.c
+++ samuelc3/LAB5/dice.c
@@ -1,5 +1,10 @@
 //Function for generating three d6 rolls
+#include "dice.h"
+#include <stdio.h>
 
 void roll_three(int* one, int* two, int* three){
-
+  *one = rand() % 6 +1;
+  *two = rand() % 6 +1;
+  *three = rand() % 6 +1;
+  return;
 }
--- samuelc3/LAB5/main.c
+++ samuelc3/LAB5/main.c
@@ -9,3 +9,28 @@
  */
 
 
+#include <stdio.h>
+#include <stdlib.h>
+#include "dice.h"
+
+
+
+int main()
+{
+  int one, two, three, seed;
+  printf("Enter seed: ");
+  scanf("%d", &seed);
+
+  srand(seed);
+  roll_three(&one, &two, &three);
+  printf("%d %d %d", one, two, three);
+
+
+  if( one==two && two==three){
+    printf("Triple!\n");
+    else if( one==two || one==three || two==three)
+      printf("Double!\n");
+    else
+      printf("/n");
+    return 0;
+  }
