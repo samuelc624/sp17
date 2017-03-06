aca6ec4836c48b162aae48b74d6b7109846cb03c
gao2 <gao2@39c386cc-3532-487e-8021-ffb347eaac5c>
2017-03-06 19:38:45 +0000

    Committing grade and test files

--- MP5/test.c
+++ MP5/test.c
@@ -8,82 +8,91 @@
 
 
 
-//Runs some unit tests for each function
-//Indicates pass or fail 
-//Input argument should indicate set_seed (0), start_game (1) or make_guess (2)
-int main (int argc, char *argv[] )
+
+int 
+main (int argc, char *argv[] )
 {
     if (argc == 1){
-	printf("Need test No, 0 for set_seed, 1 for start_game, 2 for make_guess\n");
+	printf("Enter Test No, 0 for set_seed, 1 for start_game, 2 for make_guess\n");
 	return 0;
     }
     int command;
     sscanf(argv[1],"%d",&command);
-    //Test set seed
     if (command == 0){
-	printf("*****Testing set_seed:\n");
+	int score = 0;
+	printf("Testing set_seed:\n");
 	int ret = set_seed("what?");
 	int ret2 = set_seed("12321");
 	if(ret==0 && ret2==1){
-		printf("*****set_seed return value: PASS\n");
+		printf("set_seed return value correct (10pts)\n");
+		score = score+10;		
 	}else{
-		printf("*****set_seed return value: FAIL\n");
+		printf("set_seed return value incorrect (0pts)\n");
 	}
-        //Check if random number generation correct
 	set_seed("12325");
         int test = rand();
 	if (test == 18534142){
-		printf("*****set_seed call srand: PASS\n");
+		printf("set_seed call srand correct (10pts)\n");
+		score = score+10;				
 	}else{
-		printf("*****set_seed call srand: FAIL\n");
+		printf("set_seed call srand incorrect (0pts)\n");
 	}
+	printf("*******total set_seed score: %d/20*******\n",score);
+	return score;
     }
-    //Test start game
     if (command == 1){
-	printf("*****Testing start_game:\n");
+	int score = 0;
+	printf("Testing start_game:\n");
 	int one,two,three,four;
 	srand(12321);
 	start_game(&one,&two,&three,&four);
 	int test=rand();
 	if (test == 1385017724){
-		printf("*****start_game rand() calls: PASS\n");
+		printf("start_game random number correct (15pts)\n");
+		score = score+15;
 	}else{
-		printf("*****start_game rand() calls: FAIL\n");
+		printf("start_game random number incorrect (0pts)\n");
         }
-	if (one == 8 && two == 4 && three == 8 && four == 8){
-		printf("*****start_game ouput: PASS\n");
+	if (one == 7 && two==3 && three==7 && four==7){
+		printf("start_game output correct (5pts)\n");
+		score = score+5;
 	}else{
-		printf("*****start_game output: FAIL\n");
+		printf("start_game output incorrect (0pts)\n");
 	}
+	printf("*******total start_game score: %d/20*******\n",score);
+	return score;
     }
-    //Test Make guess
-    //Test return value     
-    //Test two different guesses
     if (command == 2){
-	printf("*****Testing make_guess output:\n");
+	int score = 0;
+	printf("Testing make_guess output:\n");
 	int one,two,three,four;
 	srand(12321);
 	start_game(&one,&two,&three,&four);
 	int ret = make_guess("blahblah",&one,&two,&three,&four);
 	int ret2 = make_guess("1 2 3 4",&one,&two,&three,&four);
 	if (ret==0 && ret2==1){
-		printf("*****make_guess return value: PASS \n");
-	}else{ 
-		printf("*****make_guess return value: FAIL \n");
+		printf("make_guess return value correct (10pts) \n");
+		score = score+10;
+	}else{
+		printf("make_guess return value incorrect (0pts) \n");
         }
 	ret = make_guess("8 4 8 8",&one,&two,&three,&four);
 	if (one==8 && two==4 && three==8 && four==8){
-		printf("*****make_guess output value: PASS \n");
+		printf("make_guess output value correct (10pts)\n");
+		score = score+10;
 	}else{
-		printf("*****make_guess output value: FAIL \n");	
+		printf("make_guess output value incorrect (0pts)\n");	
 	}
-	printf("*****Testing make_guess perfect matches\n");
-	printf("*****If next line shows 4 perfect matches, then PASS\n");	
+	printf("*******make_guess output score: %d/20*******\n",score);
+    }
+    if (command==3){
+	int one,two,three,four;
 	srand(12321);
 	start_game(&one,&two,&three,&four);
 	make_guess("8 4 8 8",&one,&two,&three,&four);
-	printf("*****Testing make_guess misplaced matches\n");
-	printf("*****If next line shows 0 perfect and 2 misplaced matches, then PASS\n");	
+    }
+    if (command==4){
+	int one,two,three,four;
 	srand(12321);
 	start_game(&one,&two,&three,&four);
 	make_guess("1 8 4 1",&one,&two,&three,&four);
