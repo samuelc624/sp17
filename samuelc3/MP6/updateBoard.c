e5a1734cc2f4d94d19541e93b9b468a73c8e2d36
samuelc3 <samuelc3@39c386cc-3532-487e-8021-ffb347eaac5c>
2017-03-10 03:15:36 +0000

    mp6

--- samuelc3/MP6/updateBoard.c
+++ samuelc3/MP6/updateBoard.c
@@ -13,30 +13,153 @@
  */
 
 int countLiveNeighbor(int* board, int boardRowSize, int boardColSize, int row, int col){
-a = boardRowSize*boardColSize;
-int arr[a]
 
-for (i=0; i < boardColSize; i++) {
-	for (n = 0; i < boardRowSize; i++) {
-		if (arr[a] + 1 == 1)	{
-			alive += 1;
-		if (arr[a] - 1 == 1) {
-			alive =+ 1;
+int count=0;
+int location=(boardColSize*row+col);
+int edgerow =0;
+int edgecol =0;
+if (row==0){
+	edgerow=1;
+}
+if (row == boardRowSize){
+	edgerow=2;
+}
+if (col==0){
+	edgecol=1;
+}
+if (col == boardColSize){
+	edgecol=2;
+}
+if (edgecol==0 && edgerow==0){
+	if (*(board+location-boardColSize)==1){
+		count++;
+	}
+	if (*(board+location+boardColSize)==1){
+		count++;
+	}
+	if (*(board+location+1)==1){
+		count++;
+	}
+	if (*(board+location-1)==1){
+		count++;
+	}
+	if (*(board+location-boardColSize+1)==1){
+		count++;
+	}
+	if (*(board+location-boardColSize-1)==1){
+		count++;
+	}
+	if (*(board+location+boardColSize+1)==1){
+		count++;
+	}
+	if (*(board+location+boardColSize-1)==1){
+		count++;
+	}
+}
+else if (edgerow==1) {
+	if (*(board+location+boardColSize)==1){
+		count++;
+	}
+	if (edgecol==0){
+		if (*(board+location-1)==1){
+			count++;
 		}
-
-		col = arr[a] - boardRowSize;
-		if () {
-			/* code */
+		if (*(board+location+1)==1){
+			count++;
 		}
+		if (*(board+location+boardColSize+1)==1){
+			count++;
+		}
+		if (*(board+location+boardColSize-1)==1){
+			count++;
 		}
 	}
+	else if (edgecol==1){
+		if (*(board+location+1)==1){
+			count++;
+		}
+		if (*(board+location+boardColSize+1)==1){
+			count++;
+		}
+	}
+	else if (edgecol==2){
+		if (*(board+location-1)==1){
+			count++;
+		}
+		if (*(board+location+boardColSize-1)==1){
+			count++;
+		}
+	}
+}
+else if (edgerow==2){
+	if (*(board+location-boardColSize)==1){
+		count++;
+	}
+	if (edgecol==0){
+		if (*(board+location+1)==1){
+			count++;
+		}
+		if (*(board+location-1)==1){
+			count++;
+		}
+		if (*(board+location-boardColSize+1)==1){
+			count++;
+		}
+		if (*(board+location-boardColSize-1)==1){
+			count++;
+		}
+	}
+	else if (edgecol==1){
+		if (*(board+location+1)==1){
+			count++;
+		}
+		if (*(board+location-boardColSize+1)==1){
+			count++;
+		}
+	}
+	else if (edgecol==2){
+		if (*(board+location-1)==1){
+			count++;
+		}
+		if (*(board+location-boardColSize-1)==1){
+			count++;
+		}
+	}
+}
+else{
+	if (*(board+location-boardColSize)==1){
+ 		count++;
+ 	}
+ 	if (*(board+location+boardColSize)==1){
+ 		count++;
+ 	}
+	if (edgecol==1){
+		if (*(board+location+1)==1){
+			count++;
+		}
+		if (*(board+location-boardColSize+1)==1){
+			count++;
+		}
+		if (*(board+location+boardColSize+1)==1){
+			count++;
+		}
+	}
+else if (edgecol==2){
+	if (*(board+location-1)==1){
+ 		count++;
+ 	}
+ 	if (*(board+location-boardColSize-1)==1){
+ 		count++;
+ 	}
+ 	if (*(board+location+boardColSize-1)==1){
+ 		count++;
+ 	}
+ }
 }
 
 
 
-
-
-
+return count;
 
 
 }
@@ -50,6 +173,42 @@ for (i=0; i < boardColSize; i++) {
  * Output: board is updated with new values for next step.
  */
 void updateBoard(int* board, int boardRowSize, int boardColSize) {
+int row=0;
+int col=0;
+int neighbors=0;
+int locations=0;
+int cellstatus=-1;
+
+
+int maxsize=boardRowSize*boardColSize;
+int oldboard[maxsize];
+int i = 0;
+for (i=0;i<=maxsize;i++){
+	oldboard[i] = *(board+i);
+}
+
+for (row=0; row<=boardRowSize; row++){
+	for (col=0; col<=boardColSize; col++){
+		locations=(boardColSize*row)+col;
+		cellstatus=*(board+locations);
+		neighbors=countLiveNeighbor(board, boardRowSize, boardColSize, row, col);
+		if (cellstatus==1 && (neighbors == 2 || neighbors == 3)){
+			cellstatus = 1;
+		}
+		else if (cellstatus==0 && neighbors==3){
+			cellstatus = 1 ;
+		}
+		else{
+			cellstatus =0;
+		}
+	oldboard[locations]=cellstatus;
+	}
+}
+
+for (i=0; i<=maxsize; i++){
+	*(board+i) = oldboard[i];
+}
+
 }
 
 /*
@@ -64,4 +223,42 @@ void updateBoard(int* board, int boardRowSize, int boardColSize) {
  * return 0 if the alive cells change for the next step.
  */
 int aliveStable(int* board, int boardRowSize, int boardColSize){
+
+int maxsize=boardRowSize*boardColSize;
+int oldboard[maxsize];
+int i = 0;
+for (i=0;i<=maxsize;i++){
+	oldboard[i] = *(board+i);
+}
+
+int row=0;
+int col=0;
+int neighbors=0;
+int locations=0;
+int cellstatus=-1;
+int change=0;
+int alive;
+for (row=0; row<=boardRowSize; row++){
+	for (col=0; col<=boardColSize; col++){
+		locations=(boardColSize*row)+col;
+		cellstatus=oldboard[locations];
+		neighbors=countLiveNeighbor(board, boardRowSize, boardColSize, row, col);
+		if (cellstatus==1 && (neighbors == 2 || neighbors == 3)){
+			alive++;
+		}
+		else if (cellstatus==0 && neighbors==3) {
+			change++;
+			alive++;
+		}
+		else if (cellstatus==1 && (neighbors<2 || neighbors>3)) {
+			change++;
+		}
+	}
+}
+if (change==0 || alive==0){
+	return 1;
+}
+else{
+	return 0;
+}
 }
