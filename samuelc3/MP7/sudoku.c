2acbb0619d111aff4586889f0bab72ac9314de93
samuelc3 <samuelc3@39c386cc-3532-487e-8021-ffb347eaac5c>
2017-03-15 03:56:31 +0000

    mp7 update

--- samuelc3/MP7/sudoku.c
+++ samuelc3/MP7/sudoku.c
@@ -11,10 +11,25 @@
 int is_val_in_row(const int val, const int i, const int sudoku[9][9]) {
 
   assert(i>=0 && i<9);
-
+  int k;
+  int flag=0;
   // BEG TODO
-  
+  for (k=0;k<9;k++){
+    if (val==sudoku[i][k]){
+      flag= 1;
+      break;
+    }
+    else {
+      flag= 0;
+      continue;
+    }
+  }
+if (flag==1){
+  return 1;
+}
+else{
   return 0;
+}
   // END TODO
 }
 
@@ -23,22 +38,51 @@ int is_val_in_row(const int val, const int i, const int sudoku[9][9]) {
 int is_val_in_col(const int val, const int j, const int sudoku[9][9]) {
 
   assert(j>=0 && j<9);
-
+  int l;
+  int flag=0;
   // BEG TODO
-  
+  for (l=0; l<9; l++){
+    if (val==sudoku[l][j]){
+      flag = 1;
+      break;
+    }
+  else {
+      flag=0;
+      continue;
+    }
+  }
+if (flag==1){
+  return 1;
+}
+else{
   return 0;
+}
   // END TODO
 }
 
 // Function: is_val_in_3x3_zone
 // Return true if val already existed in the 3x3 zone corresponding to (i, j)
 int is_val_in_3x3_zone(const int val, const int i, const int j, const int sudoku[9][9]) {
-   
+
   assert(i>=0 && i<9);
-  
+	
   // BEG TODO
-  
-  return 0;
+  int n, m;
+	int flag = 0;
+
+	for(n=i-1; n<i+1; n++){
+	for(m =j-1; m<j+1; m++){
+	if(val == sudoku[i][j]){
+		flag = 1;
+	break;
+	}
+		}
+	}
+if(flag==1)   
+	return 1;
+else
+	return 0;
+
   // END TODO
 }
 
@@ -49,18 +93,38 @@ int is_val_valid(const int val, const int i, const int j, const int sudoku[9][9]
   assert(i>=0 && i<9 && j>=0 && j<9);
 
   // BEG TODO
-  return 1;
+  if (is_val_in_3x3_zone(val, i, j, sudoku)==0){
+    return 1;
+  }
+  else {return 0;}
   // END TODO
 }
 
 // Procedure: solve_sudoku
 // Solve the given sudoku instance.
 int solve_sudoku(int sudoku[9][9]) {
-
+  int i,j,val;
   // BEG TODO.
-
+  for (i=0; i<9; i++){
+    for (j=0; j<9; j++){
+      if (sudoku[i][j]==0){
+        for(val=1;val<10;val++){
+          if (is_val_valid(val, i, j, sudoku)==1){
+            sudoku[i][j]=val;
+            break;
+          }
+          else{
+            continue;
+          }
+        }
+        }
+      else{
+        continue;
+      }
+    }
+  }
   return 0;
-  // END TODO.
+ // END TODO.
 }
 
 // Procedure: print_sudoku
@@ -87,8 +151,3 @@ void parse_sudoku(const char fpath[], int sudoku[9][9]) {
   }
   fclose(reader);
 }
-
-
-
-
-
