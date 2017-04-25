c24681a2f024cad45b0258ef6396b747f1e77393
samuelc3 <samuelc3@39c386cc-3532-487e-8021-ffb347eaac5c>
2017-04-25 03:17:08 +0000

    mp12

--- samuelc3/MP12/shape.cpp
+++ samuelc3/MP12/shape.cpp
@@ -1,5 +1,5 @@
 #include "shape.hpp"
-
+#define PI  3.14159265
 
 
 //Base class
@@ -7,25 +7,80 @@
 //constructor, getName()
 //
 //Base class' constructor should be called in derived classes'
-//constructor to initizlize Shape's private variable 
-
-
+//constructor to initizlize Shape's private variable
+/*class Shape{
+  Rectangle operator+(const Rectangle& other);
+  Rectangle operator-(const Rectangle& other)
+  double getVolume();
+  double getArea();
+}
+return name_;
+*/
 
 //Rectangle
 //Please implement the member functions of Rectangle:
 //constructor, getArea(), getVolume(), operator+, operator-
 //@@Insert your code here
 
+Rectangle::Rectangle(double width, double length) : Shape("Rectangle")
+{
+length_ = length;
+width_ = width;
+}
 
 
-double Rectangle::getWidth(){return width_;}
-double Rectangle::getLength(){return length_;}
+double Rectangle::getArea(){
+  return (width_ * length_);
+}
 
+double Rectangle::getVolume(){
+  return 0;
+}
+
+Rectangle Rectangle::operator+(const Rectangle& other){
+  int dest_length = other.length_ + length_;
+  int dest_width = other.width_ + width_;
+  return Rectangle(dest_length, dest_width);
+}
+
+Rectangle Rectangle::operator-(const Rectangle& other){
+  int dest_length = other.length_ - length_;
+  int dest_width = other.width_ - width_;
+  return Rectangle(dest_length, dest_width);
+}
+
+
+double Rectangle::getWidth(){
+  return width_;
+}
+double Rectangle::getLength(){
+  return length_;
+}
 
 //Circle
 //Please implement the member functions of Circle:
 //constructor, getArea(), getVolume(), operator+, operator-
 //@@Insert your code here
+Circle::Circle(double radius) : Shape("Circle"){
+  int radius_ = radius;
+}
+double Circle::getArea(){
+  return (radius_ * radius_ * PI);
+}
+
+double Circle::getVolume(){
+  return 0;
+}
+
+Circle Circle::operator+(const Circle& other){
+  int dest_radius = other.radius_ + radius_;
+  return Circle(dest_radius);
+}
+
+Circle Circle::operator-(const Circle& other){
+  int dest_radius = other.radius_ - radius_;
+  return Circle(dest_radius);
+}
 
 double Circle::getRadius(){return radius_;}
 
@@ -49,31 +104,36 @@ double RectPrism::getHeight(){return height_;}
 double RectPrism::getLength(){return length_;}
 
 
- 
+
 // Read shapes from test.txt and initialize the objects
-// Return a vector of pointers that points to the objects 
+// Return a vector of pointers that points to the objects
 vector<Shape*> CreateShapes(char* file_name){
-	//@@Insert your code here
-	return vector<Shape*>(0, NULL); // please remeber to modify this line to return the correct value
+  int count, r, i;
+  double temp;
+  string name;
+  ifstream readfile ("test1.txt");
+  while(getline(readfile, name)){
+
+  }
+	return vector<Shape*>(0, NULL);
 }
 
-// call getArea() of each object 
+// call getArea() of each object
 // return the max area
 double MaxArea(vector<Shape*> shapes){
 	double max_area = 0;
 	//@@Insert your code here
-	
+
 	return max_area;
 }
 
 
-// call getVolume() of each object 
+// call getVolume() of each object
 // return the max volume
 double MaxVolume(vector<Shape*> shapes){
 	double max_volume = 0;
 	//@@Insert your code here
 
-	
+
 	return max_volume;
 }
-
